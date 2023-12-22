2d1b28cf09d4cddf4225d4068440ba274c5d9653
Frub_it <Frubit@protonmail.com>
2023-12-22 13:37:23 +0100

    Première version load/store par Célia
    Non testé

--- arm_load_store.c
+++ arm_load_store.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T Ã  but pÃ©dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique GÃ©nÃ©rale GNU publiÃ©e par la Free Software
+Foundation (version 2 ou bien toute autre version ultÃ©rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuÃ© car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spÃ©cifique. Reportez-vous Ã  la
+Licence Publique GÃ©nÃ©rale GNU pour plus de dÃ©tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reÃ§u une copie de la Licence Publique GÃ©nÃ©rale GNU en mÃªme
+temps que ce programme ; si ce n'est pas le cas, Ã©crivez Ã  la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+Ã‰tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 BÃ¢timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'HÃ¨res
 */
 #include "arm_load_store.h"
 #include "arm_exception.h"
@@ -26,11 +26,138 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
+
 int arm_load_store(arm_core p, uint32_t ins) {
+    //variables utilisÃ©s
+    int adr = arm_read_register(p, (unit8_t)(ins & 0x000F0000)>>16);
+    int off = 0;
+    uint8_t rdest = ((ins & 0x0000F000)>>12);
+    int i = 0x00400000;
+    int h = 1;
+	if(ins & 0x04000000){
+		i = 0x02000000;
+        h = 0;
+	}
+    int op = 0;
+    int val;
+
+
+    //CALCUL DE L'OFFSET
+    if(!(i & ins) && !h){               //word ou byte avec valeur immÃ©diate
+        off = ins & 0x00000FFF;
+    }
+    else if(!(i & ins)){                //half avec valeur immÃ©diate
+        off = (ins & 0X0000000F) + ((ins & 0x00000F00)>>4);
+    }
+    else if(!h){                        //word ou byte avec registre comme offset
+        off = arm_read_register(p, (unit8_t)(ins & 0x0F));
+        int sh = (ins & 0x00000060)>>4;
+        op = (ins & 0x00000F80)>>7;
+        if(sh == 0){off = lsl(off, op);}        //choix du shift demandÃ© pour la valeur du registre d'offset
+        else if(sh == 1){off = lsr(off, op);}
+        else if(sh == 2){off = asr(off, op);}
+        else if(op == 0){off = rxr(off);}
+        else{op = ror(off, op);}
+    }
+    else{                                //half avec registre comme offset
+        off = arm_read_register(p, (unit8_t)(ins & 0x0F));
+    }
+    
+
+
+
+    if(ins & 0x00800000){adr = adr + off;}
+    else{adr = adr - off;}              //calcul dudÃ©callage avec l'offset(bit U)
+
+
+    if(((ins & 0x01000000)>>24) == ((ins & 0x00200000)>>21)){   //mise Ã  jour du registre adresse
+        arm_write_register(p, (ins & 0x000F0000)>>16, adr);
+    }
+
+
+    int val;
+    if(ins & 0x00100000){               //ldr
+        if(h){
+            (uint16_t)(val);
+            val = arm_read_half(p, adr, &val);
+        }
+        else if(ins & 0x00400000){
+            (uint8_t)(val);
+            val = arm_read_byte(p, adr, &val);
+        }
+        else{
+            (uint32_t)(val);
+            val = arm_read_word(p, adr, &val);
+        }
+        arm_write_register(p, rdest, val);
+        return 0;
+    }
+
+    else{                               //str
+    val = arm_read_register(p, rdest);
+        if(h){
+            (uint16_t)(val);
+            arm_write_half(p, adr, val);
+            return 0;
+        }
+        else if(ins & 0x00400000){
+            (uint8_t)(val);
+            arm_write_byte(p, adr, val);
+            return 0;
+        }
+        else{
+            (uint32_t)(val);
+            arm_write_word(p, adr, val);
+            return 0;
+        }
+    }
+
     return UNDEFINED_INSTRUCTION;
 }
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {
+    //rÃ©cupÃ©ration des parties utiles de l'instruction
+    uint32_t adr = arm_read_register(p, (uint8_t)((ins & 0x000F0000)>>16));
+    uint16_t reg = ins & 0xFFFF;
+    int p = ins & 0x01000000;
+    int u = ins & 0x00800000;
+    int s = ins & 0x00400000;
+    int w = ins & 0x00200000;
+    int l = ins & 0x00100000;
+    uint8_t i = 0;
+    int j = 0;
+    if(p){
+        if(u){adr = adr + 4;}
+        else{adr = adr - 4;}
+    }
+
+
+
+    while(!j){
+        if(reg & (1<<i)){            //le registre codÃ© par le bit numÃ©ro i est concernÃ©
+            if(l && !s){arm_write_register(p, i, arm_read_word(p, adr, p->reg[i]));}
+            else if(l && s){arm_write_usr_register(p, i, arm_read_word(p, adr, p->reg[i]);)}
+            else if(!l && !s){arm_write_word(p, adr, arm_read_register(p, i))}
+            else{arm_write_word(p, adr, arm_read_usr_register(p, i));}
+            if(u){adr = adr + 4;}
+            else{adr = adr - 4;}
+        }
+
+        if(i == 15){j++;}
+        i++;
+    }
+
+
+    if((reg & (1 << 15)) && s && arm_current_mode_has_spsr(p)){
+        arm_write_cpsr(p, arm_read_spsr(p));
+        }
+
+    if(w){
+        arm_write_register(p, (unit8_t)(ins & 0x000F0000)>>16), adr);
+    }
+
+    return 0;
+
     return UNDEFINED_INSTRUCTION;
 }
 

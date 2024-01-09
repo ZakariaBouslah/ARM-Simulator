54d94e862038b866b1cb23d10ada0c91f2a84f37
Chenhao-XU <xuchenhaosasc@163.com>
2024-01-09 11:45:37 +0100

    update arm_branch_other.c not tested

--- arm_branch_other.c
+++ arm_branch_other.c
@@ -26,15 +26,30 @@ Contact: Guillaume.Huard@imag.fr
 #include <debug.h>
 #include <stdlib.h>
 
-int arm_branch(arm_core p, uint32_t ins) {
-    //WARNING : NOT TESTED YET
-    uint32_t PC = arm_read_register(p,15);
-    if (get_bit(ins, 24)){ // checking if we have to store a return address
-        arm_write_register(p, 14, PC);
+int32_t sign_extend_30(uint32_t value) {
+    if (value & (1 << 23)) {
+        value |= 0xFC000000; 
+    } else {
+        value &= 0x00FFFFFF; 
     }
-    /*TODO: Comprendre la fonction SIGN_EXTEND_30 utilisée sur la doc, et voir s'il
-            faut impérativement l'implémenter*/
-    PC = PC  +  (uint32_t)(get_bits(ins,23,0) << 2);
+    return (int32_t)value; 
+}
+
+int arm_branch(arm_core p, uint32_t ins) {
+    uint32_t PC = arm_read_register(p, 15);
+    int32_t offset;
+    uint32_t target_address;
+    int is_BL = get_bit(ins, 24);
+    int32_t signed_immed_24 = get_bits(ins, 23, 0);
+    offset = sign_extend_30(signed_immed_24) << 2; 
+
+    target_address = PC + 8 + offset; // vers l'adresse de la deuxième instruction après l'instruction en cours
+
+    if (is_BL) {
+        uint32_t LR = PC + 4; // LR est l'adresse de la prochaine instruction de l'instruction en cours
+        arm_write_register(p, 14, LR);
+    }
+    arm_write_register(p, 15, target_address);
     return 0;
 }
 
@@ -46,6 +61,16 @@ int arm_coprocessor_others_swi(arm_core p, uint32_t ins) {
 }
 
 int arm_miscellaneous(arm_core p, uint32_t ins) {
-    return UNDEFINED_INSTRUCTION;
-}
+    //Vérifiez si l'instruction est une instruction CLZ
+    if ((ins & 0x0FF000F0) == 0x016F0F10) { 
+        uint32_t Rm = get_bits(ins, 3, 0);
+        uint32_t Rd = get_bits(ins, 15, 12);
+        uint32_t value = arm_read_register(p, Rm); 
+
+        uint32_t leading_zeros = value ? __builtin_clz(value) : 32;
+
+        arm_write_register(p, Rd, leading_zeros);
+        return 0;
+    }
+    return UNDEFINED_INSTRUCTION;}
 //com

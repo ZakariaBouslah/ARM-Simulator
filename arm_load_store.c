9bd931718a36b212d6d0831477af9b4931b4f2fe
zzhang-code <67417223+zzhang-code@users.noreply.github.com>
2024-01-11 04:25:06 +0100

    Update arm_load_store.c

--- arm_load_store.c
+++ arm_load_store.c
@@ -1,32 +1,9 @@
-/*
-Armator - simulateur de jeu d'instruction ARMv5T � but p�dagogique
-Copyright (C) 2011 Guillaume Huard
-Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique G�n�rale GNU publi�e par la Free Software
-Foundation (version 2 ou bien toute autre version ult�rieure choisie par vous).
-
-Ce programme est distribu� car potentiellement utile, mais SANS AUCUNE
-GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but sp�cifique. Reportez-vous � la
-Licence Publique G�n�rale GNU pour plus de d�tails.
-
-Vous devez avoir re�u une copie de la Licence Publique G�n�rale GNU en m�me
-temps que ce programme ; si ce n'est pas le cas, �crivez � la Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-�tats-Unis.
-
-Contact: Guillaume.Huard@imag.fr
-	 B�timent IMAG
-	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'H�res
-*/
 #include "arm_load_store.h"
 #include "arm_exception.h"
 #include "arm_constants.h"
 #include "util.h"
 #include "debug.h"
 #include <assert.h>
-
 
 //L/S      :
 //      byte/word :LDR ,LDRB,LDRH,STR,STRB,STRH
@@ -104,96 +81,71 @@ uint32_t calcul_addr_scaled(arm_core p,uint32_t ins){//P A5-22
 }
 //A5-18 p458 used to calculate the address for load and store word or byte
 int is_immediate_offset(uint32_t ins){//offset +-12  A5-18 p458
-    if(get_bits(ins,27,25)==0x010){
+    if(get_bit(ins,25)==0){
         return 1;
     }
     return 0;
 }
 int is_register_offset(uint32_t ins){//offset +- arm_read_register(p,Rm)
-    if(get_bits(ins,27,25) == 0x011 && get_bits(ins,11,4)==0 ){
+    if(get_bit(ins,25) == 1 && get_bits(ins,11,4)==0 ){
         return 1;
     }
     return 0;
 }
 int is_scaled_offset(uint32_t ins){
-    if(get_bits(ins,27,25) == 0x011 && get_bits(ins,11,4)!=0){
+    if(get_bit(ins,25) == 1 && get_bits(ins,11,4)!=0){
         return 1;
     }
     return 0;
 }
 //A5-20 p460
-uint32_t determiner_addr(arm_core p,uint32_t ins){
+void determiner_addr(arm_core p,uint32_t ins,uint32_t *addr){
     int bitP = get_bit(ins,24);
     int bitW = get_bit(ins,21);
     //int bitI = get_bit(ins,25);
-    uint32_t addr;
     if(bitP==0 && bitW ==0){//post index 
         if(is_immediate_offset(ins)){//A5-28 p468
-            addr = arm_read_register(p,bits_rn(ins));
+            *addr = arm_read_register(p,bits_rn(ins));
+            arm_write_register(p,bits_rn(ins),calcul_addr_immediate(p,ins));//+-12
         }else if(is_register_offset(ins)){//A5-30
-            addr = arm_read_register(p,bits_rn(ins));
+            *addr = arm_read_register(p,bits_rn(ins));
+            arm_write_register(p,bits_rn(ins),calcul_addr_register(p,ins));//+-rm
         }else if(is_scaled_offset(ins)){//A5-31
-            addr = arm_read_register(p,bits_rn(ins));
+            *addr = arm_read_register(p,bits_rn(ins));
+            arm_write_register(p,bits_rn(ins),calcul_addr_scaled(p,ins));//+-index
         }
     }else if(bitP ==1 && bitW == 0){//immediate offset
         if(is_immediate_offset(ins)){//A5-20 p460
-            addr = calcul_addr_immediate(p,ins);
+            *addr = calcul_addr_immediate(p,ins);
         }else if(is_register_offset(ins)){//A5-21
-            addr = calcul_addr_register(p,ins);
+            *addr = calcul_addr_register(p,ins);
         }else if(is_scaled_offset(ins)){//A5-22
-            addr = calcul_addr_scaled(p,ins);
-        }
-    }else if(bitP == 0 && bitW == 1){
-        return UNDEFINED_INSTRUCTION;
-        
-    }else if(bitP == 1 && bitW == 1){//pre index
-        if(is_immediate_offset(ins)){//A5-24 p464
-            addr = calcul_addr_immediate(p,ins);
-        }else if(is_register_offset(ins)){//A5-25
-            addr = calcul_addr_register(p,ins);
-        }else if(is_scaled_offset(ins)){//A5-26
-            addr = calcul_addr_scaled(p,ins);
-        }
-    }
-    return addr;
-}
-//A5-24 address written back to the base register Rn
-uint32_t write_back_rn(arm_core p,uint32_t ins,uint32_t addr){
-    int bitP = get_bit(ins,24);
-    int bitW = get_bit(ins,21);
-    //int bitI = get_bit(ins,25);
-    //uint32_t addr;
-    //ifconditionpassed then
-    if(bitP==0 && bitW ==0){//post index 
-        if(is_immediate_offset(ins)){//A5-28 p468
-            arm_write_register(p,bits_rn(ins),calcul_addr_immediate(p,ins));//+-12
-        }else if(is_register_offset(ins)){//A5-30
-            arm_write_register(p,bits_rn(ins),calcul_addr_register(p,ins));//+-rm
-        }else if(is_scaled_offset(ins)){//A5-31
-            arm_write_register(p,bits_rn(ins),calcul_addr_scaled(p,ins));//+-index
+            *addr = calcul_addr_scaled(p,ins);
         }
     }else if(bitP == 1 && bitW == 1){//pre index
         if(is_immediate_offset(ins)){//A5-24 p464
-            arm_write_register(p,bits_rn(ins),addr);
+            *addr = calcul_addr_immediate(p,ins);
+            arm_write_register(p,bits_rn(ins),*addr);
         }else if(is_register_offset(ins)){//A5-25
-            arm_write_register(p,bits_rn(ins),addr);
+            *addr = calcul_addr_register(p,ins);
+            arm_write_register(p,bits_rn(ins),*addr);
         }else if(is_scaled_offset(ins)){//A5-26
-            arm_write_register(p,bits_rn(ins),addr);
+            *addr = calcul_addr_scaled(p,ins);
+            arm_write_register(p,bits_rn(ins),*addr);
         }
     }
-    return 0;
 }
 
 //Mode 3
 //A5-33 p473 used to calculate the address for load and store  halfword  load signed byte or load and store doubleword
 int is_misimmediate_offset(uint32_t ins){
-    if(get_bits(ins,27,25)==0x000 && bitB(ins) == 1){
+    if(bitB(ins) == 1){
         return 1;
     }
     return 0;
 }
 int is_misregister_offset(uint32_t ins){
-    if(get_bits(ins,27,25)==0x000 && bitB(ins) == 0){
+    if(bitB(ins) == 0){
         return 1;
     }
     return 0;
@@ -225,88 +177,65 @@ uint32_t calcul_addr_misregister(arm_core p,uint32_t ins){//A5-36
     }
 }
 //bit P W determiner pre/post index A5-34
-uint32_t determiner_mis_addr(arm_core p,uint32_t ins){
-    uint32_t addr;
+void determiner_mis_addr(arm_core p,uint32_t ins,uint32_t *addr){
+
     if(bitP(ins)==0 && bitW(ins) ==0){//post index 
         if(is_misimmediate_offset(ins)){//A5-39
-            addr = calcul_addr_misimmediate(p,bits_rn(ins));
+            *addr = calcul_addr_misimmediate(p,bits_rn(ins));
+            arm_write_register(p,bits_rn(ins),calcul_addr_misimmediate(p,ins));//+-8
         }else if(is_misregister_offset(ins)){//A5-40
-            addr = calcul_addr_misregister(p,bits_rn(ins));
+            *addr = calcul_addr_misregister(p,bits_rn(ins));
+            arm_write_register(p,bits_rn(ins),calcul_addr_misregister(p,ins));//+-rm
         }  
     }else if(bitP(ins) ==1 && bitW(ins) == 0){//unchanged offset addressing A5-35
         if(is_misimmediate_offset(ins)){
-            addr = calcul_addr_misimmediate(p,ins);
+            *addr = calcul_addr_misimmediate(p,ins);
         }else if(is_misregister_offset(ins)){
-            addr = calcul_addr_misregister(p,ins);
+            *addr = calcul_addr_misregister(p,ins);
         }    
-    }else if(bitP(ins) == 0 && bitW(ins) == 1){
-        return UNDEFINED_INSTRUCTION;
-        
     }else if(bitP(ins) == 1 && bitW(ins) == 1){//pre index A5-37
         if(is_misimmediate_offset(ins)){
-            addr = calcul_addr_misimmediate(p,ins);
+            *addr = calcul_addr_misimmediate(p,ins);
+            arm_write_register(p,bits_rn(ins),*addr);
             //rn = addr
         }else if(is_misregister_offset(ins)){
-            addr = calcul_addr_misregister(p,ins);
+            *addr = calcul_addr_misregister(p,ins);
+            arm_write_register(p,bits_rn(ins),*addr);
             //rn = addr
         } 
     }
-    return addr;
-}
-//apres visiter les donne A5-24 address written back to teh base register Rn
-uint32_t write_back_mis_rn(arm_core p,uint32_t ins,uint32_t addr){
-    int bitP = get_bit(ins,24);
-    int bitW = get_bit(ins,21);
-    //int bitI = get_bit(ins,25);
-    //uint32_t addr;
-    //ifconditionpassed then
-    if(bitP==0 && bitW ==0){//post index 
-        if(is_immediate_offset(ins)){//A5-39
-            arm_write_register(p,bits_rn(ins),calcul_addr_misimmediate(p,ins));//+-8
-        }else if(is_register_offset(ins)){//A5-40
-            arm_write_register(p,bits_rn(ins),calcul_addr_misregister(p,ins));//+-rm
-        }
-    }else if(bitP == 1 && bitW == 1){//pre index
-        if(is_misimmediate_offset(ins)){
-            arm_write_register(p,bits_rn(ins),addr);
-            //rn = addr
-        }else if(is_misregister_offset(ins)){
-            arm_write_register(p,bits_rn(ins),addr);
-            //rn = addr
-        }    
-    }
-    return 0;
-}
 
+}
 
 
 //instrction load/store
 int instruction_ldr(arm_core p, uint32_t ins){//A4-43 p193
-    //uint8_t rd = get_bits(ins,15,12);
-    // uint32_t offset = get_bits(ins,11,0);
-    uint32_t address = determiner_addr(p,ins);
     uint32_t word;
-    if(arm_read_word(p,address,&word)==0){
-        if(bits_rd(ins) == 15){
-            arm_write_register(p,bits_rd(ins),word & 0xFFFFFFFE);
-            uint8_t t_bit = get_bit(word,0);
-            if(t_bit == 0){
-                arm_write_cpsr(p,clr_bit(arm_read_cpsr(p),5));
-            }else
-                arm_write_cpsr(p,set_bit(arm_read_spsr(p),5));
-        }else{
-            arm_write_register(p,bits_rd(ins),word);
-        }
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
+    //MemoryAccess(B-bit, E-bit)
+    
+    arm_read_word(p, address, &word);//data = Memory[address,4]
+    if(bits_rd(ins) == 15){
+        arm_write_register(p,bits_rd(ins),word & 0xFFFFFFFE);
+        uint8_t t_bit = get_bit(word,0);
+        if(t_bit == 0){
+            arm_write_cpsr(p,clr_bit(arm_read_cpsr(p),5));
+        }else
+            arm_write_cpsr(p,set_bit(arm_read_spsr(p),5));
+    }else{
+        arm_write_register(p,bits_rd(ins),word);
     }
-    write_back_rn(p,ins,address);
+    
     return 0;
 }
 int instruction_ldrb(arm_core p, uint32_t ins){//A4-46
     uint8_t byte;
-    uint32_t address = determiner_addr(p,ins);
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
     arm_read_byte(p,address,&byte);
     arm_write_register(p,bits_rd(ins),byte);
-    write_back_rn(p,ins,address);
+
     return 0;
 }
 
@@ -314,17 +243,18 @@ int instruction_ldrb(arm_core p, uint32_t ins){//A4-46
 int instruction_ldrbt(arm_core p, uint32_t ins){
 
     uint8_t byte;
-    uint32_t address = determiner_addr(p,ins);
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
     arm_read_byte(p,address,&byte);
     arm_write_usr_register(p,bits_rd(ins),byte);
-    write_back_rn(p,ins,address);
     return 0;
 }
 //A4-50 Mode 3 loads a pair fo ARM registers from two consecutive words of memory
 int instruction_ldrd(arm_core p, uint32_t ins){
     //a completer
     uint32_t word1,word2;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
     if((bits_rd(ins) % 2) == 0 && bits_rd(ins) != 14 && get_bits(address,1,0) == 0b00 && get_bit(address,2) == 0){
         arm_read_word(p,address,&word1);
         arm_read_word(p,address+4,&word2);
@@ -333,14 +263,15 @@ int instruction_ldrd(arm_core p, uint32_t ins){
     }
     // else
     //     return UNDEFINED_INSTRUCTION;
-    write_back_mis_rn(p,ins,address);
+
     return 0;
 }
 
 //A4-54 Mode3 loads a halfword from memory and zero-extends it to a 32-bit word.
 int instruction_ldrh(arm_core p, uint32_t ins){ 
     uint16_t half;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_mis_addr(p,ins,&address);
     //CP15_reg1_Ubit 
     if(get_bit(address,0) == 0){
     half = arm_read_half(p,address,&half);
@@ -349,27 +280,29 @@ int instruction_ldrh(arm_core p, uint32_t ins){
     
 
     arm_write_register(p,bits_rd(ins),half);
-    write_back_mis_rn(p,ins,address);
+
     return 0;
 }
 //A4-56 Mode3 loads a byte from memory and sign-extends the byte to a 32-bit word.
 int instruction_ldrsb(arm_core p, uint32_t ins){
     uint8_t byte;
     uint32_t extend_word;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_mis_addr(p,ins,&address);
 
     arm_read_byte(p,address,&byte);
     extend_word = (uint32_t)byte;
     arm_write_register(p,bits_rd(ins),extend_word);
 
-    write_back_mis_rn(p,ins,address);
+
     return 0;
 }
 //A4-58 Mode3 loads a halfword from memory and sign-extends the halfword to a 32-bit word.
 int instruction_ldrsh(arm_core p, uint32_t ins){
     uint16_t half;
     uint32_t extend_word;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_mis_addr(p,ins,&address);
 
     if(get_bit(address,0) == 0){
         arm_read_half(p,address,&half);
@@ -380,7 +313,7 @@ int instruction_ldrsh(arm_core p, uint32_t ins){
     extend_word = (uint32_t)half;
     arm_write_register(p,bits_rd(ins),extend_word);
 
-    write_back_mis_rn(p,ins,address);
+
     
     return 0;
 }
@@ -388,40 +321,43 @@ int instruction_ldrsh(arm_core p, uint32_t ins){
 //A4-60 Mode2 loads a word from memory (User mode)
 int instruction_ldrt(arm_core p, uint32_t ins){
     uint32_t word;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_addr(p,ins,&address);
 
     arm_read_word(p,address,&word);
     arm_write_usr_register(p,bits_rd(ins),word);
 
-    write_back_rn(p,ins,address);
+
     return 0;
 }
 
 //A4-193 p 343
 //mode 2 stores a word from a register to memory
 int instruction_str(arm_core p, uint32_t ins){
-    uint32_t address = determiner_addr(p,ins);  
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
     arm_write_word(p,address,arm_read_register(p,bits_rd(ins)));
-    write_back_rn(p,ins,address);
+
     return 0;
 }
 
 //mode 2 stores a byte from the least significant byte of a register to memory
 int instruction_strb(arm_core p, uint32_t ins){//A4-195
-    uint32_t address = determiner_addr(p,ins);  
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
     arm_write_word(p,address,arm_read_register(p,bits_rd(ins)&0xFF));  //rd[7:0]
-    write_back_rn(p,ins,address);
+
     return 0;
 }
 
 //A4-197 mode 2 stores a byte from the least significant byte of a register to memory (USER mode)
 int instruction_strbt(arm_core p, uint32_t ins){
     //uint8_t byte;
-    uint32_t address;
-    address = determiner_addr(p,ins);
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
 
     arm_write_byte(p,address,arm_read_register(p,bits_rd(ins))&0xFF);
-    write_back_rn(p,ins,address);
+
     
     return 0;
 }
@@ -429,7 +365,8 @@ int instruction_strbt(arm_core p, uint32_t ins){
 //A4-199 Mode 3 stores a pair of ARM registers to two consecutive words of memory.
 int instruction_strd(arm_core p, uint32_t ins){
     uint32_t word1,word2;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_mis_addr(p,ins,&address);
     if((bits_rd(ins) % 2) == 0 && bits_rd(ins) != 14 && get_bits(address,1,0) == 0b00 && get_bit(address,2) == 0){
         word1 = arm_read_register(p,bits_rn(ins));
         word2 = arm_read_register(p,bits_rn(ins)+1);
@@ -439,14 +376,14 @@ int instruction_strd(arm_core p, uint32_t ins){
     }else
         return UNDEFINED_INSTRUCTION;
 
-    write_back_mis_rn(p,ins,address);
     return 0;
 }
 
 //A4-204 mode 3 stores a halfword from the least significant halfword of a register to memory.
 int instruction_strh(arm_core p, uint32_t ins){//A4-204 p354
     uint16_t half;
-    uint32_t address = determiner_mis_addr(p,ins);
+    uint32_t address = 0;
+    determiner_mis_addr(p,ins,&address);
     //if(bitU(ins)==0)
     if(get_bit(address,0) == 0b0){
         half = arm_read_register(p,bits_rd(ins));
@@ -454,20 +391,18 @@ int instruction_strh(arm_core p, uint32_t ins){//A4-204 p354
         return UNDEFINED_INSTRUCTION;
 
     arm_write_half(p,address,half);
-    write_back_mis_rn(p,ins,address);
     return 0;
 }
 
 //A4-206 Mode 2 stores a word from a register to memory. (USER mode)
 int instruction_strt(arm_core p, uint32_t ins){
     uint32_t word;
-    uint32_t address;
-    address = determiner_addr(p,ins);
+    uint32_t address = 0;
+    determiner_addr(p, ins,&address);
 
     word = arm_read_register(p,bits_rd(ins));
     arm_write_word(p,address,word);
 
-    write_back_rn(p,ins,address);
     return 0;
 }
 
